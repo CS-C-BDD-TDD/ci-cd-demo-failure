f400ee0a4e680c33c4bb028e318631dd630d6662
Anne Dalton <annekathryndalton@gmail.com>
2018-10-22 15:37:35 -0400

    Update HumanreviewApiController.java

--- src/main/java/gov/dhs/nppd/humanreview/service/HumanreviewApiController.java
+++ src/main/java/gov/dhs/nppd/humanreview/service/HumanreviewApiController.java
@@ -30,11 +30,12 @@ public class HumanreviewApiController implements HumanreviewApi {
 	
 	Connection conn = null;
 	try {
- 		 conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=steve&password=blue"); // Noncompliant
-  String uname = "steve";
-  String password = "blue";
-  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-        "user=" + uname + "&password=" + password); // Noncompliant
+ 	    conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=steve&password=blue"); // Noncompliant
+            String uname = "steve";
+            String password = "blue";
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+            "user=" + uname + "&password=" + password); // Noncompliant
+	}
 
   java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Noncompliant
 
