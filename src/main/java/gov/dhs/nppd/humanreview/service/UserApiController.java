8445db485433f2815c44a4759c87b210cd879624
Anne Dalton <annekathryndalton@gmail.com>
2018-10-23 12:36:38 -0400

    Update UserApiController.java

--- src/main/java/gov/dhs/nppd/humanreview/service/UserApiController.java
+++ src/main/java/gov/dhs/nppd/humanreview/service/UserApiController.java
@@ -28,8 +28,6 @@ import io.swagger.annotations.ApiParam;
 public class UserApiController implements UserApi {
 
 	private final NativeWebRequest request;
-	
-	private static final String PASSWORD = "hardcodedpassword";
 
 	@org.springframework.beans.factory.annotation.Autowired
 	public UserApiController(NativeWebRequest request, AuthCredentialsRepository authCredentialsRepository) {
